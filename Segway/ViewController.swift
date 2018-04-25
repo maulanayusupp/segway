17ee802a49d290085555c1208d6f79313b23b294
Maulana Yusup Abdullah <adeneling@gmail.com>
2018-04-25 11:33:28 +0700

    Initial Commit

---
+++ Segway/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Segway
+//
+//  Created by Maulana Yusup A on 25/04/18.
+//  Copyright © 2018 Maulana Yusup A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
